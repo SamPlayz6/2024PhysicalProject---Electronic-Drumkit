9143e42bc6e1ae92f0f6be2c97e9f345db59605c
SamPlayz6 <samddunning@hotmail.co.uk>
2024-06-16 16:59:20 +0100

    Drum sound added and assigned to the piezo force hit

--- SinglePiezoMIDIOuput/SinglePiezoMIDIOuput.ino
+++ SinglePiezoMIDIOuput/SinglePiezoMIDIOuput.ino
@@ -2,26 +2,17 @@
 
 int sensoroutput = 1; // the analog pin connected to the sensor
 int THRESHOLD = 500;
+// Acoustic Snare on MIDI Channel 10 (internally, Channel 9)
+uint8_t channel = 1;  // MIDI channels are 0-15, so Channel 10 is 9
+uint8_t pitch = 38;   // Acoustic Snare drum pitch
+uint8_t velocity = 127;  // Maximum hit velocity
 
-void noteOn(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOn = { 0x09, 0x90 | channel, pitch, velocity };
-  MidiUSB.sendMIDI(noteOn);
-}
 
-void noteOff(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOff = { 0x08, 0x80 | channel, pitch, velocity };
-  MidiUSB.sendMIDI(noteOff);
-}
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
 }
-
-// First parameter is the event type (0x0B = control change).
-// Second parameter is the event type, combined with the channel.
-// Third parameter is the control number number (0-119).
-// Fourth parameter is the control value (0-127).
 
 void controlChange(byte channel, byte control, byte value) {
   midiEventPacket_t event = { 0x0B, 0xB0 | channel, control, value };
@@ -34,17 +25,26 @@ void loop() {
     if (value <= THRESHOLD)                    // function to check voltage level from sensor
     {
       Serial.println("Sending note on");
-      noteOn(1, 48, 64);  // Channel 0, middle C, normal velocity
+      noteOn(channel, pitch, velocity);  // Channel 0, middle C, normal velocity
       digitalWrite(LED_BUILTIN, HIGH);
       MidiUSB.flush();
       delay(2000);
     }
     else {
-      noteOff(1, 48, 64);  // Channel 0, middle C, normal velocity
+      noteOff(channel, pitch, velocity);  // Channel 0, middle C, normal velocity
       digitalWrite(LED_BUILTIN, LOW);
       MidiUSB.flush();
     }
-
-
   }
-  // controlChange(0, 10, 65); // Set the value of controller 10 on channel 0 to 65
+
+void noteOn(uint8_t channel, uint8_t pitch, uint8_t velocity) {
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+  MidiUSB.flush(); // Make sure to send the message immediately
+}
+
+void noteOff(uint8_t channel, uint8_t pitch, uint8_t velocity) {
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, 0}; // Velocity should be 0 for noteOff
+  MidiUSB.sendMIDI(noteOff);
+  MidiUSB.flush(); // Make sure to send the message immediately
+}
